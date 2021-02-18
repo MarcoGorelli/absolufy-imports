6f3928ce98865551f400fae073e05fcccf85037c
Marco Gorelli <marcogorelli@protonmail.com>
2021-02-18 21:37:31 +0000

    wip

--- abs_imports.py
+++ abs_imports.py
@@ -9,23 +9,44 @@ from typing import Tuple
 
 
 class Visitor(ast.NodeVisitor):
-    def __init__(self, parts: Sequence[str]) -> None:
+    def __init__(self, parts: Sequence[str], keep_local_imports_relative: bool) -> None:
         self.parts = parts
         self.to_replace: MutableMapping[int, Tuple[str, str]] = {}
+        self.keep_local_imports_relative = keep_local_imports_relative
 
     def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
         level = node.level
-        if level == 0 and node.module is not None:
+
+        if not self.keep_local_imports_relative and level == 0 and node.module is not None:
+            # Already absolute import.
+            self.generic_visit(node)
+            return
+
+        if self.keep_local_imports_relative and level == 1:
+            # Should be relative, and already is.
+            self.generic_visit(node)
+            return
+
+        should_be_relative = self.keep_local_imports_relative and (
+            '.'.join(self.parts[:-1]) == node.module
+        )
+        if should_be_relative:
+            self.to_replace[node.lineno] = (
+                rf'(from\s+){".".join(self.parts[:-1])}',
+                f'\\1.',
+            )
             self.generic_visit(node)
             return
 
         absolute_import = '.'.join(self.parts[:-level])
 
         if node.module is None:
+            # e.g. from . import b
             self.to_replace[
                 node.lineno
             ] = (rf'(from\s+){"."*level}', f'\\1{absolute_import}')
         else:
+            # e.g. from .b import c
             module = node.module
             self.to_replace[
                 node.lineno
@@ -37,11 +58,7 @@ class Visitor(ast.NodeVisitor):
         self.generic_visit(node)
 
 
-def absolute_imports(file: str, application_directories: str) -> None:
-    srcs = (
-        Path(i).resolve()
-        for i in application_directories.split(':')
-    )
+def absolute_imports(file: str, srcs: Sequence[str], *, keep_local_imports_relative=False) -> None:
     relative_path = Path(file)
     for i in srcs:
         try:
@@ -54,7 +71,7 @@ def absolute_imports(file: str, application_directories: str) -> None:
         txt = fd.read()
     tree = ast.parse(txt)
 
-    visitor = Visitor(relative_path.parts)
+    visitor = Visitor(relative_path.parts, keep_local_imports_relative)
     visitor.visit(tree)
 
     if not visitor.to_replace:
@@ -75,9 +92,18 @@ def main(argv: Optional[Sequence[str]] = None) -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--application-directories', default='.:src')
     parser.add_argument('files', nargs='*')
+    parser.add_argument('--keep-local-imports-relative', action='store_true')
     args = parser.parse_args(argv)
+
+    srcs = [
+        str(Path(i).resolve())
+        for i in args.application_directories.split(':')
+    ]
+
     for file in args.files:
-        absolute_imports(file, args.application_directories)
+        absolute_imports(
+            file, srcs, keep_local_imports_relative=args.keep_local_imports_relative,
+        )
 
 
 if __name__ == '__main__':
---
+++ tests/keep_locals_relative_test.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+from abs_imports import main
+
+
+def test_main(tmpdir):
+    os.mkdir(os.path.join(str(tmpdir), 'mypackage'))
+    os.mkdir(os.path.join(str(tmpdir), 'mypackage', 'mysubpackage'))
+    tmp_file = os.path.join(
+        str(tmpdir), 'mypackage', 'mysubpackage', 'bar.py',
+    )
+    shutil.copy(
+        os.path.join('tests', 'data', 'bar.py'), tmp_file,
+    )
+
+    cwd = os.getcwd()
+    os.chdir(str(tmpdir))
+    try:
+        main(
+            (
+                '--keep-local-imports-relative',
+                os.path.join('mypackage', 'mysubpackage', 'bar.py'),
+            ),
+        )
+    finally:
+        os.chdir(cwd)
+
+    with open(tmp_file) as fd:
+        result = fd.read()
+
+    expected = (
+        'from . import B\n'
+        'from mypackage.foo import T\n'
+        'from .bar import D\n'
+        'from . import O\n'
+        '\n'
+        'print(T)\n'
+        'print(D)\n'
+    )
+    assert result == expected
