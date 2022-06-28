dabd4bdc95c32f26ce8bc5f66af32ed280277c0f
ClimenteA <climente.alin@gmail.com>
2022-06-28 08:50:08 +0300

    chore: tests

---
+++ package/n2/n3/n3_module.py
@@ -0,0 +1,21 @@
+from ..n2_module import func3
+
+# from ..n2_module import func3
+# from package.n2.n2_module import func3
+
+
+def func4(param1: str, param2: dict):
+    func3(param1, param2)
+    return param1, param2
+
+
+async def cofunc4():
+    return "ok"
+
+
+class Class4:
+    def __init__(self) -> None:
+        pass
+
+    def method4(self):
+        return "classic response"
---
+++ tests/package_for_conversion_test.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+from absolufy_imports import main, get_module_paths, to_absolute_imports
+
+
+# pytest -s -v tests/package_for_conversion_test.py
+
+
+# pytest -s -v tests/package_for_conversion_test.py::test_get_module_paths
+def test_get_module_paths():
+
+    module_paths = get_module_paths(package_name="package")
+
+    assert sorted(module_paths) == sorted(
+        ["./package/n2/n2_module.py", "./package/n2/n3/n3_module.py"]
+    )
+
+    for path in module_paths:
+        assert os.path.exists(path)
+
+
+# pytest -s -v tests/package_for_conversion_test.py::test_to_absolute_imports
+def test_to_absolute_imports():
+
+    shutil.copytree("./package", "./pkg")
+
+    to_absolute_imports("pkg")
+
+    with open("./pkg/n2/n3/n3_module.py", "r") as f:
+        flines = f.readlines()
+
+    assert "from pkg.n2.n2_module import func3" == flines[0].strip()
+
+    shutil.rmtree("./pkg")
+
+
+# pytest -s -v tests/package_for_conversion_test.py::test_main_with_package_flag
+def test_main_with_package_flag():
+
+    shutil.copytree("./package", "./pkg")
+
+    main(
+        (
+            "--package",
+            "pkg",
+        ),
+    )
+
+    with open("./pkg/n2/n3/n3_module.py", "r") as f:
+        flines = f.readlines()
+
+    assert "from pkg.n2.n2_module import func3" == flines[0].strip()
+
+    shutil.rmtree("./pkg")
